22423a4bf3a49610dfdb75faae616afcebc02b92
sofin <sofin@DESKTOP-E9GOOQI>
2023-01-24 14:59:20 -0500

    created TaskPage object in BasePage Class

--- src/test/java/com/acti/tests/BaseTest.java
+++ src/test/java/com/acti/tests/BaseTest.java
@@ -5,11 +5,13 @@ import org.testng.annotations.BeforeMethod;
 import com.acti.base.DriverScript;
 import com.acti.page.HomePage;
 import com.acti.page.LoginPage;
+import com.acti.page.TaskPage;
 
 public class BaseTest extends DriverScript
 {
 	LoginPage lp = new LoginPage();
 	HomePage hp = new HomePage();
+	TaskPage tp = new TaskPage();
 	
 	@BeforeMethod
 	public void Setup()
@@ -17,6 +19,7 @@ public class BaseTest extends DriverScript
 		initApplication();
 	    lp = new LoginPage();
 	    hp = new HomePage();
+	    tp = new TaskPage();
 	}
 	
 	@AfterMethod
