18f3ae423fd7f4b53b919f43c4f50184d47b9c43
sofin <sofin@DESKTOP-E9GOOQI>
2023-01-24 23:20:02 -0500

    Added locators for delete customer and added methods for each elements

--- src/main/java/com/acti/page/TaskPage.java
+++ src/main/java/com/acti/page/TaskPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.acti.base.DriverScript;
+import com.acti.utils.Helper;
 
 public class TaskPage extends DriverScript
 {
@@ -16,6 +17,13 @@ public class TaskPage extends DriverScript
 	@FindBy(xpath = "//textarea[@placeholder='Enter Customer Description']") WebElement textareaCustomerDescription;
 	@FindBy(xpath = "//div[text()='Create Customer']") WebElement buttoncreatecustomer;
 	@FindBy(xpath = "//span[@class='innerHtml']") WebElement msgSuccess;
+	@FindBy(xpath = "(//input[@placeholder='Start typing name ...'])[1]") WebElement textboxstarttyping;
+	@FindBy(xpath = "//span[@class='highlightToken']") WebElement searchedCustomer;
+	@FindBy(xpath = "(//div[@class='editButton'])[18]") WebElement buttonEdit;
+	@FindBy(xpath = "(//div[@class='action'])[1]") WebElement buttonAction;
+	@FindBy(xpath = "//div[@class='taskManagement_customerPanel']//div[@class='title'][normalize-space()='Delete']")
+	WebElement buttonDelete;
+	@FindBy(xpath = "(//span[@class='submitTitle buttonTitle'])[1]") WebElement buttondeletepermantely;
 	
 	
 	//*************************************Page Initialization*******************************************
@@ -56,5 +64,38 @@ public class TaskPage extends DriverScript
 	public String getSuccessMessage()
 	{
 		return msgSuccess.getText();
+	}
+	
+	public void entercustomertype(String customername)
+	{
+		textboxstarttyping.sendKeys(customername);
+	}
+	
+	public void clicksearchedcustomer()
+	{
+		searchedCustomer.click();
+	}
+	
+	public void clickeditbutton()
+	{
+		buttonEdit.click();
+		Helper.sleep();
+	}
+	
+	public void clickactionbutton()
+	{
+		buttonAction.click();
+		Helper.sleep();
+	}
+	
+	public void clickdeletebutton()
+	{
+		buttonDelete.click();
+	}
+	
+	public void clickdeletebuttonperm()
+	{
+		buttondeletepermantely.click();
+		Helper.sleep();
 	}
 }
