46218a2072967e0753352b589d9385dac4cb2d88
sofin <sofin@DESKTOP-E9GOOQI>
2023-01-24 23:15:23 -0500

    Added ExtentReports

--- src/test/java/com/acti/tests/BaseTest.java
+++ src/test/java/com/acti/tests/BaseTest.java
@@ -2,17 +2,37 @@ package com.acti.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
 import com.acti.base.DriverScript;
 import com.acti.page.HomePage;
 import com.acti.page.LoginPage;
 import com.acti.page.TaskPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class BaseTest extends DriverScript
 {
-	LoginPage lp = new LoginPage();
-	HomePage hp = new HomePage();
-	TaskPage tp = new TaskPage();
+	LoginPage lp;
+	HomePage hp;
+	TaskPage tp;
+	protected static ExtentReports report;
+	protected static ExtentTest logger;
 	
+	
+	
+	@BeforeSuite
+	public void setupReport()
+	{
+		//create 2 objects here
+		ExtentHtmlReporter extent = new ExtentHtmlReporter("./atreports/autoindex.html");//any name can be given for autoindex
+		report = new ExtentReports();
+		report.attachReporter(extent);
+	}
+	
+
+
 	@BeforeMethod
 	public void Setup()
 	{
@@ -25,6 +45,7 @@ public class BaseTest extends DriverScript
 	@AfterMethod
 	public void tearDown()
 	{
+		 report.flush();//flush is added coz logger variable is added in global and which is used for logging
 		 closeApplication();
 	}
 
